63630fe9202038a96aae20b615db0e66f5bdab6c
drwiner <drwiner131@gmail.com>
2017-08-21 15:08:36 -0600

    added backup code file

--- assign_cam_to_clip.cs
+++ assign_cam_to_clip.cs
@@ -15,8 +15,6 @@ public class assign_cam_to_clip : MonoBehaviour {
     public List<CinemachineVirtualCamera> prim_cams;
     public List<GameObject> special_shots;
 
-    //public TimelineAsset timeline;
-    // public GameObject main_camera_object;
     // Use this for initialization
     void Start() {
         cam_dict = new Dictionary<string, CinemachineVirtualCamera>();
@@ -30,38 +28,28 @@ public class assign_cam_to_clip : MonoBehaviour {
         PlayableDirector director = GetComponent<PlayableDirector>();
         
 
-        // var timeline = GetComponent<TimelineAsset>();
-        //director.Pause();
-        //  director.playableAsset.CreatePlayable.
-
-        //Sucess:
-        //        PlayableBinding track = director.playableAsset.outputs.First(c => c.streamName == "Cinemachine Track");
-        //        director.SetGenericBinding(track.sourceObject, settable_camera);
-
-        // timeline = (TimelineAsset)Resources.Load("RightAngleShotTimeline");
-        // timeline = new TimelineAsset();
+        
 
         TimelineAsset timeline = (TimelineAsset)ScriptableObject.CreateInstance("TimelineAsset");
 
         TrackAsset track = timeline.CreateTrack<CinemachineTrack>(null, "trackname");
 
 
-      //  CinemachineMixer
-        
-        // use this 
-       // TrackAsset track = timeline.GetRootTrack(0);
-
+      
         var clip = track.CreateDefaultClip();
         clip.start = 0.0;
         clip.duration = 4;
         clip.displayName = "justcreated";
 
 
+        // Camera 1
         CinemachineVirtualCamera left_angle = (CinemachineVirtualCamera)prim_cams[0];
         var cinemachineShot = clip.asset as CinemachineShot;
         cinemachineShot.VirtualCamera.exposedName = UnityEditor.GUID.Generate().ToString();
         director.SetReferenceValue(cinemachineShot.VirtualCamera.exposedName, left_angle);
 
+
+        // Camera 2
         CinemachineVirtualCamera right_angle = (CinemachineVirtualCamera)prim_cams[1];
         var clip2 = track.CreateDefaultClip();
         clip2.start = 4;
@@ -70,59 +58,21 @@ public class assign_cam_to_clip : MonoBehaviour {
         cinemachineShot2.VirtualCamera.exposedName = UnityEditor.GUID.Generate().ToString();
         director.SetReferenceValue(cinemachineShot2.VirtualCamera.exposedName, right_angle);
 
+
+        // Camera 3
         GameObject overhead = (GameObject) special_shots[0];
         TrackAsset ctrack = timeline.CreateTrack<ControlTrack>(null, "control_track");
         var clip3 = ctrack.CreateDefaultClip();
         clip3.start = 8;
         clip3.duration = 8;
-        
         var controlshot =  clip3.asset as ControlPlayableAsset;
         controlshot.sourceGameObject.exposedName = UnityEditor.GUID.Generate().ToString();
         director.SetReferenceValue(controlshot.sourceGameObject.exposedName, overhead);
 
-        //AnimationMixerPlayable mixerPlayable = AnimationMixerPlayable.Create(director.playableGraph, 2);
-        //  director.playableGraph.Connect(clip, 0, mixerPlayable, 0);
-        //director.playableGraph.Connect(clip2, 0, mixerPlayable, 1);
-        //        var track = timeline.CreateTrack(CinemachineTrack, )//
-
-        //      var track = timeline.CreateTrack<CinemachineTrack>(timeline, "just_made");
-        //    director.SetGenericBinding()
-        // TrackAsset track = timeline.outputs.First(c => c.streamName == "Cinemachine Track");
-        //   var track = timeline.outputs[0].sourceObject;
-        //director.playableAsset.CreatePlayable(director.playableGraph, this.gameObject);
-
-        //  track.Set
-        //track.
-        //TimelineAsset this_timeline = director.GetComponent<TimelineAsset>();
-        //CinemachineTrack this_timeline = director.GetComponent<CinemachineTrack>();
-        //  director.
-
-        //TimelineAsset this_timeline = this.GetComponent<TimelineAsset>();
-        //this_timeline.
-        //TrackAsset first_track = this_timeline.GetOutputTrack(0);
-
-        //   CinemachineTrack first_track = this_timeline.GetClips()
-
-
-        //CinemachineTrack first_track = this.GetComponent<CinemachineTrack>();
-
-        // The binding fields for the first track
-        //var binding = director.GetGenericBinding(first_track);
-
-        //foreach (var c in track.GetClips())
-        //{
-        //    var cinemachineShot = c.asset as CinemachineShot;
-
-        //    // remember the virtual camera (an exposed reference) refers to an object in the scene
-        //    cinemachineShot.VirtualCamera.exposedName = UnityEditor.GUID.Generate().ToString();
-        //    // for now, set each clip to have the settable camera.
-        //    director.SetReferenceValue(cinemachineShot.VirtualCamera.exposedName, settable_camera);
-        //}
+        // Set cinemachine brain as track's game object
         director.SetGenericBinding(track, main_camera_object);
-        //director.SetGenericBinding(ctrack, )
-        //director.GetGenericBinding(this);
-        //this.GetComponent<TimelineAsset>().
-        // var first_param = this.GetComponent<TimelineAsset>().CreateTrack(ControlTrack,)
+        
+        // Set it to play when ready.
         director.Play(timeline);
     }
 
---
+++ backup_code.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backup_code : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        // var timeline = GetComponent<TimelineAsset>();
+        //director.Pause();
+        //  director.playableAsset.CreatePlayable.
+
+        //Sucess:
+        //        PlayableBinding track = director.playableAsset.outputs.First(c => c.streamName == "Cinemachine Track");
+        //        director.SetGenericBinding(track.sourceObject, settable_camera);
+
+        // timeline = (TimelineAsset)Resources.Load("RightAngleShotTimeline");
+        // timeline = new TimelineAsset();
+
+        //  CinemachineMixer
+
+        // use this 
+        // TrackAsset track = timeline.GetRootTrack(0);
+
+
+        //AnimationMixerPlayable mixerPlayable = AnimationMixerPlayable.Create(director.playableGraph, 2);
+        //  director.playableGraph.Connect(clip, 0, mixerPlayable, 0);
+        //director.playableGraph.Connect(clip2, 0, mixerPlayable, 1);
+        //        var track = timeline.CreateTrack(CinemachineTrack, )//
+
+        //      var track = timeline.CreateTrack<CinemachineTrack>(timeline, "just_made");
+        //    director.SetGenericBinding()
+        // TrackAsset track = timeline.outputs.First(c => c.streamName == "Cinemachine Track");
+        //   var track = timeline.outputs[0].sourceObject;
+        //director.playableAsset.CreatePlayable(director.playableGraph, this.gameObject);
+
+        //  track.Set
+        //track.
+        //TimelineAsset this_timeline = director.GetComponent<TimelineAsset>();
+        //CinemachineTrack this_timeline = director.GetComponent<CinemachineTrack>();
+        //  director.
+
+        //TimelineAsset this_timeline = this.GetComponent<TimelineAsset>();
+        //this_timeline.
+        //TrackAsset first_track = this_timeline.GetOutputTrack(0);
+
+        //   CinemachineTrack first_track = this_timeline.GetClips()
+
+
+        //CinemachineTrack first_track = this.GetComponent<CinemachineTrack>();
+
+        // The binding fields for the first track
+        //var binding = director.GetGenericBinding(first_track);
+
+        //foreach (var c in track.GetClips())
+        //{
+        //    var cinemachineShot = c.asset as CinemachineShot;
+
+        //    // remember the virtual camera (an exposed reference) refers to an object in the scene
+        //    cinemachineShot.VirtualCamera.exposedName = UnityEditor.GUID.Generate().ToString();
+        //    // for now, set each clip to have the settable camera.
+        //    director.SetReferenceValue(cinemachineShot.VirtualCamera.exposedName, settable_camera);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
