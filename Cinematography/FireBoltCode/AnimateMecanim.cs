02f50cea1f0d18b680bfa8ac1ebaecd9489f17fc
drwiner <drwiner131@gmail.com>
2018-02-28 11:49:51 -0700

    added legacy Firebolt code for reference, project in dev mode

---
+++ Cinematography/FireBoltCode/AnimateMecanim.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections;
+using CM = CinematicModel;
+
+namespace Assets.scripts
+{
+    public class AnimateMecanim : FireBoltAction
+    {
+        private string actorName;
+        private GameObject actor;
+        private string animName;
+        private string stateName;
+        private Animator animator;
+        private AnimationClip animation;
+        private AnimationClip state;
+        AnimatorOverrideController overrideController;
+        private int stopTriggerHash;
+        private bool loop;
+        private static readonly string animationToOverride = "_87_a_U1_M_P_idle_Neutral__Fb_p0_No_1";
+        private static readonly string stateToOverride = "state";
+        bool assignEndState = false;
+
+        public static bool ValidForConstruction(string actorName, CM.Animation animation)
+        {
+            if (string.IsNullOrEmpty(actorName) || animation == null || string.IsNullOrEmpty(animation.FileName))
+                return false;
+            return true;
+        }
+
+
+        public AnimateMecanim(float startTick, float endTick, string actorName, string animName, bool loop, string endingName) :
+            base(startTick, endTick)
+        {
+            this.actorName = actorName;
+            this.animName = animName;
+			this.loop = loop;
+            this.assignEndState = !string.IsNullOrEmpty(endingName);
+            this.stateName = endingName; 
+            stopTriggerHash = Animator.StringToHash("stop");
+        }
+
+        public override bool Init()
+        {
+            //short circuit if this has clearly been initialized before
+            if(animator && overrideController && animation && 
+                (!assignEndState ||(assignEndState && state)))
+            {
+                assignAnimations();
+                animator.runtimeAnimatorController = overrideController;
+                actor.SetActive(true);
+                return true;
+            }
+
+            if (!findAnimations()) return false;
+            //get the actor this animate action is supposed to affect
+            if(actor == null &&
+               !getActorByName(actorName, out actor))
+            {
+                Debug.LogError("actor[" + actorName + "] not found.  cannot animate");
+                return false;
+            }
+
+            //get the actor's current animator if it exists
+            animator = actor.GetComponent<Animator>();
+            if (animator == null)
+            {
+                animator = actor.AddComponent<Animator>();
+            }
+            animator.applyRootMotion = false;
+
+            //find or make an override controller
+            if (animator.runtimeAnimatorController is AnimatorOverrideController)
+            {
+                overrideController = (AnimatorOverrideController) animator.runtimeAnimatorController;
+            }
+            else
+            {
+                overrideController = new AnimatorOverrideController();
+                overrideController.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>("AnimatorControllers/Generic");
+                animator.runtimeAnimatorController = overrideController;
+            }
+
+            assignAnimations();
+            return true;
+        }
+
+        private void assignAnimations()
+        {
+            overrideController[animationToOverride] = animation;
+            if (assignEndState)
+                overrideController[stateToOverride] = state;
+        }
+
+        private bool findAnimations()
+        {
+            //find animations
+            if (ElPresidente.Instance.GetActiveAssetBundle().Contains(animName))
+            {
+                animation = ElPresidente.Instance.GetActiveAssetBundle().LoadAsset<AnimationClip>(animName);
+
+                if (animation == null)
+                {
+                    Debug.LogError(string.Format("unable to find animation [{0}] in asset bundle[{1}]", animName, ElPresidente.Instance.GetActiveAssetBundle().name));
+                    return false;
+                }
+                animation.wrapMode = loop ? WrapMode.Loop : WrapMode.Once;                
+            }
+            else
+            {
+                Debug.Log(string.Format("asset bundle [{0}] does not contain animation[{1}]", ElPresidente.Instance.GetActiveAssetBundle().name, animName));
+                return false;
+            }
+
+            if (!string.IsNullOrEmpty(stateName) && ElPresidente.Instance.GetActiveAssetBundle().Contains(stateName))
+            {
+                assignEndState = true;
+                state = ElPresidente.Instance.GetActiveAssetBundle().LoadAsset<AnimationClip>(stateName);
+                if (state == null)
+                {
+                    Debug.LogError(string.Format("unable to find animation [{0}] in asset bundle[{1}]", stateName, ElPresidente.Instance.GetActiveAssetBundle().name));
+                    if (state == null) return false;
+                }
+            }
+            else if (!string.IsNullOrEmpty(stateName) && !ElPresidente.Instance.GetActiveAssetBundle().Contains(stateName))
+            {
+                Debug.Log(string.Format("should have looked up a state animation[{0}] and failed ", stateName));
+                return false;
+            }
+            return true;
+        }
+
+        public override void Undo()
+		{
+		}
+
+        public override void Skip()
+        {
+            animator.SetTrigger(stopTriggerHash);
+        }
+
+        public override void Execute(float currentTime) 
+        {
+		    //let it roll          
+            float at = Mathf.Repeat ((currentTime - startTick)/1000, animation.length);
+            animator.CrossFade( "animating", 0, 0, at/animation.length);
+	    }
+
+        public override void Stop()
+        {
+            animator.SetTrigger(stopTriggerHash);
+        }
+    }
+}
---
+++ Cinematography/FireBoltCode/CameraActionFactory.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using LN.Utilities;
+using Impulse.v_1_336;
+using UintT = Impulse.v_1_336.Intervals.Interval<Impulse.v_1_336.Constants.ValueConstant<uint>, uint>;
+using UintV = Impulse.v_1_336.Constants.ValueConstant<uint>;
+using CM = CinematicModel;
+using Oshmirto;
+
+namespace Assets.scripts
+{
+    public class CameraActionFactory
+    {
+        private static readonly string cameraName = "Pro Cam";
+        private static readonly string cameraRig = "Rig";
+        public static uint endDiscourseTime = 0;
+        public static Dictionary<string, ushort> lenses = new Dictionary<string, ushort>() 
+        { 
+            {"12mm",0}, {"14mm",1}, {"16mm",2}, {"18mm",3}, {"21mm",4}, {"25mm",5}, {"27mm",6}, 
+            {"32mm",7}, {"35mm",8}, {"40mm",9}, {"50mm",10}, {"65mm",11}, {"75mm",12}, {"100mm",13},
+            {"135mm",14}, {"150mm",15}, {"180mm",16}
+        };
+
+        public static Dictionary<string, ushort> fStops = new Dictionary<string, ushort>()
+        {
+            {"1.4",0}, {"2",1}, {"2.8",2}, {"4",3}, {"5.6",4}, {"8",5}, {"11",6}, {"16",7}, {"22",8}
+        };
+
+        public static void CreateActions(Story<UintV, UintT, IIntervalSet<UintV, UintT>> story, CM.CinematicModel cinematicModel, string cameraPlanPath, 
+                                                     out CameraActionList cameraActionList, out FireBoltActionList discourseActionList)
+        {
+            cameraActionList = new CameraActionList();
+            discourseActionList = new FireBoltActionList();
+            CameraPlan cameraPlan = Oshmirto.Parser.Parse(cameraPlanPath);
+
+            if(cinematicModel.MillisPerTick != 1)
+            {
+                scaleTime(cinematicModel.MillisPerTick, cameraPlan);
+            }
+            
+
+            uint currentDiscourseTime = 0;
+            float previousStoryTimeOffset = 0;
+            foreach (Block block in cameraPlan.Blocks)
+            {
+                float blockStartTime = Single.MaxValue;
+                float blockEndTime = Single.MinValue;
+                foreach (var fragment in block.ShotFragments)
+                {
+                    uint fragmentStartTime = currentDiscourseTime++;
+                    uint fragmentEndTime = fragmentStartTime + fragment.Duration;
+                    
+                    if (fragmentStartTime < blockStartTime) //assumes same time scale for discourse and story
+                        blockStartTime = fragmentStartTime;                                     
+                    if (fragmentEndTime > blockEndTime)
+                        blockEndTime = fragmentEndTime;
+
+                    cameraActionList.Add(new ShotFragmentInit(fragmentStartTime, cameraRig, fragment.Anchor, fragment.Height, fragment.Pan,
+                        fragment.Lens, fragment.FStop, fragment.Framings, fragment.Direction, fragment.Angle, fragment.FocusPosition));
+
+                    var movementStartTime = fragmentStartTime + 1; //force moves to sort after inits
+                    RotateRelative rotateWith = null; //collect all the relative rotations together for the camera so we can avoid representational nightmares
+                    foreach (var movement in fragment.CameraMovements)
+                    {
+                        switch (movement.Type) 
+                        {
+                            case CameraMovementType.Dolly :
+                                switch (movement.Directive)
+                                {
+                                    case(CameraMovementDirective.With):
+                                        cameraActionList.Add(new TranslateRelative(movement.Subject, movementStartTime, fragmentEndTime, cameraRig, false, true, false));
+                                        break;
+                                    case(CameraMovementDirective.To):
+                                        Vector2 destination;
+                                        if (movement.Subject.TryParsePlanarCoords(out destination))
+                                        {
+                                            cameraActionList.Add(new Translate(movementStartTime, fragmentEndTime, cameraRig,
+                                                                                null, new Vector3Nullable(destination.x,null,destination.y)));
+                                        }
+                                        break;
+                                }                               
+                                break;
+                            case CameraMovementType.Crane :
+                                switch (movement.Directive)
+                                {
+                                    case CameraMovementDirective.With:
+                                        break;
+                                    case CameraMovementDirective.To:
+                                        cameraActionList.Add(new Translate(movementStartTime, fragmentEndTime, cameraRig,
+                                                                            null, new Vector3Nullable(null, float.Parse(movement.Subject), null)));
+                                        break;
+                                }
+                                break;
+                            case CameraMovementType.Pan:
+                                switch (movement.Directive)
+                                {
+                                    case CameraMovementDirective.With:
+                                        if (rotateWith != null)
+                                        {
+                                            rotateWith.AppendAxis(movement.Subject, cameraRig, true, false);
+                                        }
+                                        else
+                                        {
+                                            rotateWith = new RotateRelative(movement.Subject, movementStartTime, fragmentEndTime, cameraRig,
+                                                                               true, false);
+                                        }
+                                       
+                                        break;
+                                    case CameraMovementDirective.To:
+                                        cameraActionList.Add(new Rotate(movementStartTime, fragmentEndTime, cameraRig, new Vector3Nullable(null, float.Parse(movement.Subject), null),null));
+                                        break;
+                                }
+                                break;
+                            case CameraMovementType.Tilt:
+                                switch(movement.Directive)
+                                {
+                                    case CameraMovementDirective.With: // will this co-execute with pan-with? not currently
+                                        if (rotateWith != null)
+                                        {
+                                            rotateWith.AppendAxis(movement.Subject, cameraRig, false, true);
+                                        }
+                                        else
+                                        {
+                                            rotateWith = new RotateRelative(movement.Subject, movementStartTime, fragmentEndTime, cameraRig,
+                                                                               false, true);
+                                        }
+                                        break;
+                                    case CameraMovementDirective.To:
+                                        cameraActionList.Add(new Rotate(movementStartTime, fragmentEndTime, cameraRig, new Vector3Nullable(float.Parse(movement.Subject), null, null),null));
+                                        break;
+                                }
+                                break;
+                            case CameraMovementType.Focus:
+                                switch (movement.Directive)
+                                {
+                                    case CameraMovementDirective.With:
+                                        cameraActionList.Add(new Focus(movementStartTime, fragmentEndTime, cameraName, movement.Subject, true));
+                                        break;
+                                }
+                                break;
+                        }
+                    }
+                    if(rotateWith!=null)
+                        cameraActionList.Add(rotateWith);
+                    // Shake it off
+                    if(fragment.Shake > float.Epsilon)
+                        cameraActionList.Add(new Shake(movementStartTime, fragmentEndTime, cameraName, fragment.Shake));
+
+                    currentDiscourseTime = fragmentEndTime;
+                }
+                if (block.StoryTime.HasValue)
+                {
+                    float currentStoryTimeOffset = block.StoryTime.Value - blockStartTime;
+                    discourseActionList.Add(new SetStoryTime(currentStoryTimeOffset, previousStoryTimeOffset, blockStartTime, blockEndTime));
+                    previousStoryTimeOffset = block.StoryTime.Value;
+                }
+            }
+            cameraActionList.EndDiscourseTime = currentDiscourseTime;
+        }
+
+        private static void scaleTime(uint millisPerTick, CameraPlan cameraPlan)
+        {
+            
+            foreach(var block in cameraPlan.Blocks)
+            {
+                if (block.StoryTime.HasValue)
+                    block.StoryTime = block.StoryTime.Value * millisPerTick;
+                foreach(var fragment in block.ShotFragments)
+                {
+                    fragment.Duration *= millisPerTick;
+                }
+            }
+        }
+
+    }
+}
